23f9b98830d1da7e84767c9d218cb32a9202f17f
Halada <halada.martas@seznam.cz>
2019-01-31 12:34:33 +0100

    commit one

---
+++ localStorage.js
@@ -0,0 +1,17 @@
+var Storage;
+function statistika(id){
+    var pocet;
+    console.log(id, "text");
+    if(typeof(Storage) !== "undefined"){
+        if(localStorage.getItem(id) == "undefined"){
+
+            pocet = 1;
+            localStorage.setItem(id, pocet);
+        }else{
+            console.log(id);
+            pocet = localStorage.getItem(id);
+            pocet++;
+            localStorage.setItem(id, pocet);
+        }
+    }
+}
